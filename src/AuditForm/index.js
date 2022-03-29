b48ee9aa67c5a550dda64a7cfc13841292464f31
Michael Chuang <mhychuang@gmail.com>
2022-03-29 11:32:10 -0400

    last comment

--- src/AuditForm/index.js
+++ src/AuditForm/index.js
@@ -1,1 +1,8 @@
-export {default} from './AuditForm' ;
+export {default} from './AuditForm' ;  //from that file
+
+// import AuditForm2 from './AuditForm2';
+
+// export AuditForm2; 
+
+// //app.js
+// import {AuditForm2} from './AuditFrom'
